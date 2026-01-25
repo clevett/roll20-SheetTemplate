a1e9c4d088fe14aeb0d65ca0c788d992542ea80a
CassieLevett <cassielevett@gmail.com>
2026-01-25 17:40:58 +0000

    delete the old drag and drop

--- src/ts/drag_and_drop/process_data_arrays.ts
+++
@@ -1,8 +0,0 @@
-const processDataArrays = (array, callback) => {
-  if (array === undefined) {
-    return;
-  }
-  const parsed = typeof array === "string" ? parseJSON(array) : array;
-  const map = parsed.map((e) => callback(e));
-  return map?.reduce((acc, val) => ({ ...acc, ...val }));
-};
