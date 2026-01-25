3865bb329c339f466193cd7f3914c268d48f8157
CassieLevett <cassielevett@gmail.com>
2026-01-25 18:04:23 +0000

    update css overrides and types

--- src/Roll20.d.ts
+++ src/Roll20.d.ts
@@ -1,32 +1,34 @@
-// Type definitions for Roll20 built-in functions and variables
-// Definitions by: Karl Erik Hofseth https://github.com/Karlinator
-
 declare type EventInfo = {
+  newValue: string;
+  previousValue: string;
+  removedInfo?: { [key: string]: AttrValue };
   sourceAttribute: string;
   sourceType: string;
-  previousValue: string;
-  newValue: string;
-  removedInfo: string;
+  triggerName: string;
 };
 
-declare type AttributeContent = string | number | boolean;
+declare type AttrValue = string | number | boolean;
 
-declare type Attrs = { [key: string]: AttributeContent };
+declare type Attrs = { [key: string]: AttrValue };
+
+declare type Attributes = string[];
+
+declare type Ids = string[];
 
 declare function getAttrs(
-  attributes: string[],
+  attributes: Attributes,
   callback?: (values: { [key: string]: string }) => void
 ): void;
 
 declare function setAttrs(
-  values: { [key: string]: AttributeContent },
+  values: { [key: string]: AttrValue },
   options?: { silent?: boolean },
   callback?: (values: { [key: string]: string }) => void
 ): void;
 
 declare function getSectionIDs(
   section_name: string,
-  callback: (values: string[]) => void
+  callback: (values: Ids) => void
 ): void;
 
 declare function generateRowID(): string;
@@ -37,7 +39,22 @@ declare function getTranslationByKey(key: string): string | false;
 
 declare function getTranslationLanguage(): string;
 
-declare function setDefaultToken(values: { [key: string]: string }): void;
+declare type TokenValues = {
+  bar1_link?: string | number;
+  bar1_max?: string | number;
+  bar1_value?: string | number;
+  bar2_link?: string | number;
+  bar2_max?: string | number;
+  bar2_value?: string | number;
+  bar3_link?: string | number;
+  bar3_max?: string | number;
+  bar3_value?: string | number;
+  width: number;
+  height: number;
+  light_radius?: number;
+};
+
+declare function setDefaultToken(values: TokenValues): void;
 
 declare function on(
   event: string,
@@ -52,7 +69,7 @@ declare type CompendiumAttributes = {
     blobs: { [key: string]: unknown };
     expansion: number;
     //- This custom data added by the compendium owner
-    [key: string]: AttributeContent;
+    [key: string]: string;
   };
   content: string;
 };
