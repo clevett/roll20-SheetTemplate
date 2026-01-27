72ca5f40a78b39a43a6e55c956397a839acf73ea
CassieLevett <cassielevett@gmail.com>
2026-01-27 03:04:27 +0000

    save tab setup

--- src/ts/event_handlers.ts
+++ src/ts/event_handlers.ts
@@ -1,34 +1,8 @@
-//Calculate a simple derived attribute
-// characteristics.movement.forEach((attr: string) => {
-//   on(`change:${attr}`, () => {
-//     getAttrs(characteristics.movement, (values) => {
-//       const {strength, dexterity} = values;
-//       setAttrs({
-//         movement: sumIntegers([strength, dexterity]) / 2,
-//       });
-//     });
-//   });
-// });
+//    console.log(`%c On change ${tab}`, "color: blue; font-weight: bold;");
 
-//Calculate all the values of a repeating section. Useful for calculating weights, totals, etc.
-// on(`change:repeating_events:weight`, ({ triggerName }) => {
-//   const repeatingRow = getReprowid(triggerName);
-
-//   getSectionIDs("events", (idArray) => {
-//     let rows = [];
-//     idArray.forEach((id) =>
-//       rows.push(`repeating_events_${id}_weight`)
-//     );
-
-//     getAttrs(rows, (values) => {
-//       const parsedNums = parseIntegers(values);
-//       const weightValues = Object.values(parsedNums);
-//       const sum = sumIntegers(weightValues);
-
-//       setAttrs({
-//         weight_total: sum,
-//         [`${repeatingRow}_total_weight`]: sum,
-//       });
-//     });
-//   });
-// });
+const viewTabs = ["style_guide", "character", "npc"];
+viewTabs.forEach((tab) => {
+  on(`clicked:${tab}`, () => {
+    setAttrs({ sheet_type: tab });
+  });
+});
